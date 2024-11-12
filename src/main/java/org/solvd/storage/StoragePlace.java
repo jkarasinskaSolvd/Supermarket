7774b87982f2a886e8ba4ab09f6d6a9b560860c9
jkarasinskaSolvd <jkarasinska@laba.solvd.com>
2024-11-12 13:27:46 +0100

    Overrided toString(), hashCode() and equals() in StoragePlace added.

--- src/main/java/org/solvd/storage/StoragePlace.java
+++ src/main/java/org/solvd/storage/StoragePlace.java
@@ -6,6 +6,7 @@ import org.solvd.product.StorageMethod;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public abstract class StoragePlace {
     protected String name;
@@ -47,5 +48,34 @@ public abstract class StoragePlace {
 
     public List<Product> getProducts() {
         return products;
+    }
+
+    @Override
+    public String toString() {
+        return "StoragePlace{" +
+                "name='" + name + '\'' +
+                ", category=" + category +
+                ", products=" + products +
+                ", storageMethod=" + storageMethod +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if(getClass() != o.getClass()) return false;
+        if(hashCode() != o.hashCode()) return false;
+        StoragePlace storagePlace = (StoragePlace) o;
+        if(this.name != storagePlace.name) return false;
+        if(this.category != storagePlace.category) return false;
+        if(this.products != storagePlace.products) return false;
+        if(this.storageMethod != storagePlace.storageMethod) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + category.hashCode() + products.hashCode() + storageMethod.hashCode();
     }
 }
